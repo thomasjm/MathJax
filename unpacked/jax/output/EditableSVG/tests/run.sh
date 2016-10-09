f1cd6d3127fc39e86ba0447515052a9de4e5bf4c
Tom McLaughlin <pyro777@gmail.com>
2016-10-08 20:06:37 -0700

    Thing

--- unpacked/jax/output/EditableSVG/tests/run.sh
+++ unpacked/jax/output/EditableSVG/tests/run.sh
@@ -3,4 +3,4 @@
 virtualenv mathjax_test
 source mathjax_test/bin/activate
 pip install -r requirements.txt
-py.test selenium_tests.py
+py.test
--- unpacked/jax/output/EditableSVG/tests/run_headless.sh
+++ unpacked/jax/output/EditableSVG/tests/run_headless.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 virtualenv mathjax_test
-. mathjax_test/bin/activate
+source mathjax_test/bin/activate
 pip install -r requirements.txt
-xvfb-run --auto-servernum --server-args="-screen 0 1920x1080x24" py.test selenium_tests.py
+xvfb-run --auto-servernum --server-args="-screen 0 1920x1080x24" py.test
